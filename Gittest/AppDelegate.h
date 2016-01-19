ae78adb89b859db157fe921c6b93b8d6d6b622bb
Adam Joniec <adamjoniec@adams-mac-mini.local>
2016-01-19 10:47:03 +0100

    Initial Commit

---
+++ Gittest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gittest
+//
+//  Created by Adam Joniec on 19/01/16.
+//  Copyright © 2016 Adam Joniec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
